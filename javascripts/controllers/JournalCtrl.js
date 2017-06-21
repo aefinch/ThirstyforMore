101c10bcdfe82c5e3ab2d30e78e9f3ec12375bd9
Andrea Finch <aefinch@vt.edu>
2017-06-20 21:09:23 -0500

    dated notes working, note collection setup

--- javascripts/controllers/JournalCtrl.js
+++ javascripts/controllers/JournalCtrl.js
@@ -1,5 +1,6 @@
-app.controller("JournalCtrl", function ($location, $scope, $routeParams, $rootScope, DrinkFactory){
+app.controller("JournalCtrl", function ($location, $scope, $routeParams, $rootScope, DrinkFactory, NoteFactory){
 	let bevId = $routeParams.id;
+    $scope.edit = false;
 	let loadDrinkInfo = (bevId) => {
 		$scope.drink = [];
 		DrinkFactory.getDrinks(bevId, "bevId").then((drinkz) => {
@@ -18,18 +19,26 @@ app.controller("JournalCtrl", function ($location, $scope, $routeParams, $rootSc
     };
     loadDrinkInfo(bevId);
 
-    let addNewDrink = (drink) => {
-    	DrinkFactory.postNewDrink(drink).then((results) => {
-    		$scope.drink = {};
+    let addNewDrink = (drink, comment) => {
+        NoteFactory.postNewNote(comment).then((results) => {
+            $scope.userNote = {};
+        }).catch((error) => {
+            console.log("Add note error", error);
+        });
+        DrinkFactory.postNewDrink(drink).then((results) => {
+            $scope.drink = {};
             $location.url(`/journal/listView`);
-    	}).catch((error) => {
-    		console.log("Add error", error);
-    	});
+        }).catch((error) => {
+            console.log("Add error", error);
+        });
     };
     $scope.buttonType="Submit";
     $scope.submitDrink = (drink) => {
 	    $scope.drink.uid = $rootScope.user.uid;
         $scope.drink.tastedDate = new Date();
-	    addNewDrink($scope.drink);
+        $scope.userNote.bevId = $scope.drink.bevId;
+        $scope.userNote.uid = $rootScope.user.uid;
+        $scope.userNote.date = $scope.drink.tastedDate;
+	    addNewDrink($scope.drink, $scope.userNote);
 	};
 });
